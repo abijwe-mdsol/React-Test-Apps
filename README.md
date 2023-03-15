d1a2acafa6efa5812a26a9168a01a958b7c54b7f
Akash Bijwe <abijwe@mdsol.com>
2023-03-15 17:14:35 +0530

    12. Describe added multiple and nested + coverage script added

--- README.md
+++ README.md
@@ -3,4 +3,10 @@
 RAFC - to create a react component
 
 
-React testing - 9
+React testing - 12
+
+Notes:
+------------------------
+1. test.only() // To only test this testcase.
+2. test.skip() // To skip this testcase.
+3. 1 file is one test suites.
--- React Testing/testing_with_jest/src/components/greet-second/greet-second.test.tsx
+++ React Testing/testing_with_jest/src/components/greet-second/greet-second.test.tsx
@@ -4,14 +4,39 @@
 import { render, screen } from "@testing-library/react"
 import GreetSecond from "./greet-second"
 
-test('Greet second render correctly', () => {
-  render(<GreetSecond />);
-  const testText = screen.getByText('Hello');
-  expect(testText).toBeInTheDocument();
-})
 
-test('Greet renders with a name', () => {
-  render(<GreetSecond name="Akash" />)
-  const textElement = screen.getByText('Hello Akash');
-  expect(textElement).toBeInTheDocument();
+// describe.only('Greet component', () => {
+// describe.skip('Greet component', () => {
+describe('Greet component', () => {
+
+  test('second render correctly', () => {
+    render(<GreetSecond />);
+    const testText = screen.getByText('Hello');
+    expect(testText).toBeInTheDocument();
+  })
+
+  test('renders with a name', () => {
+    render(<GreetSecond name="Akash" />)
+    const textElement = screen.getByText('Hello Akash');
+    expect(textElement).toBeInTheDocument();
+  })
+
+  // Nested Describe 
+  describe('Nested Greet', () => {
+    test('renders with a name', () => {
+      render(<GreetSecond name="Akash" />)
+      const textElement = screen.getByText('Hello Akash');
+      expect(textElement).toBeInTheDocument();
+    })
+  })
+
+});
+
+// Multiple Describe
+describe('Multiple Describe Greet', () => {
+  test('renders with a name', () => {
+    render(<GreetSecond name="Akash" />)
+    const textElement = screen.getByText('Hello Akash');
+    expect(textElement).toBeInTheDocument();
+  })
 })
